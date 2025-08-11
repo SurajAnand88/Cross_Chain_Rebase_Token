bbb55bbd6f03fa184dfaed2a7ca11799574ab32f
SurajAnand88 <surajbloodstone@gmail.com>
2025-08-11 13:34:04 +0530

    added configurePool Script

---
+++ script/ConfigurePool.s.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {Script} from "forge-std/Script.sol";
+
+contract ConfigurePoolScript is Script {
+    function run(
+        address localPool,
+        uint64 remoteChainSelector,
+        address remotePool,
+        address remoteToken,
+        bool outboundRateLimiterIsEnabled,
+        uint128 outboundRateLimiterCapacity,
+        uint128 outboundRateLimiterRate,
+        bool inboundRateLimiterIsEnabled,
+        uint128 inboundRateLimiterCapacity,
+        uint128 inboundRateLimiterRate
+    ) {}
+}
