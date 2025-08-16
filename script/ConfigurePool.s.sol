bf76502045f3599bc57fdef11a10172e840098df
SurajAnand88 <surajbloodstone@gmail.com>
2025-08-16 13:24:16 +0530

    configure pool script

--- script/ConfigurePool.s.sol
+++ script/ConfigurePool.s.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.24;
 
 import {Script} from "forge-std/Script.sol";
+import {TokenPool} from "lib/ccip/contracts/src/v0.8/ccip/pools/TokenPool.sol";
+import {RateLimiter} from "../lib/ccip/contracts/src/v0.8/ccip/libraries/RateLimiter.sol";
 
 contract ConfigurePoolScript is Script {
     function run(
@@ -15,5 +17,30 @@ contract ConfigurePoolScript is Script {
         bool inboundRateLimiterIsEnabled,
         uint128 inboundRateLimiterCapacity,
         uint128 inboundRateLimiterRate
-    ) {}
+    ) public {
+        vm.startBroadcast();
+
+        TokenPool.ChainUpdate[] memory chainsToAdd = new TokenPool.ChainUpdate[](1);
+        bytes[] memory remotePoolAddresses = new bytes[](1);
+
+        remotePoolAddresses[0] = abi.encode(remotePool);
+        chainsToAdd[0] = TokenPool.ChainUpdate({
+            remoteChainSelector: remoteChainSelector,
+            remotePoolAddresses: remotePoolAddresses,
+            remoteTokenAddress: abi.encode(remoteToken),
+            outboundRateLimiterConfig: RateLimiter.Config({
+                isEnabled: outboundRateLimiterIsEnabled,
+                capacity: outboundRateLimiterCapacity,
+                rate: outboundRateLimiterRate
+            }),
+            inboundRateLimiterConfig: RateLimiter.Config({
+                isEnabled: inboundRateLimiterIsEnabled,
+                capacity: inboundRateLimiterCapacity,
+                rate: inboundRateLimiterRate
+            })
+        });
+
+        TokenPool(localPool).applyChainUpdates(new uint64[](0), chainsToAdd);
+        vm.stopBroadcast();
+    }
 }
